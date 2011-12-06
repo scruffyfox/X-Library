3fde2d79837996fa10ef255c166eaaa3422bfdc0
Callum Taylor <callum@3sidedcube.com>
2011-12-06 17:03:41 +0000

    Added comments

--- src/x/lib/Random.java
+++ src/x/lib/Random.java
@@ -1,7 +1,14 @@
 package x.lib;
 
+/**
+ * An extention to java Random
+ */
 public class Random extends java.util.Random
 {
+	/**
+	 * Gets a random integer within the range of a 32bit hex color. Alpha is always 255.
+	 * @return The generated hex color
+	 */
 	public int nextHexColor()
 	{
 		int randomInt = nextInt(0xffffff);
