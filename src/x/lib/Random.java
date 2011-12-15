18eb4c7d794261992c29c80a443d75a2a5d06e5a
Callum Taylor <callum@3sidedcube.com>
2011-12-15 12:07:30 +0000

    Added random int generator for range

--- src/x/lib/Random.java
+++ src/x/lib/Random.java
@@ -14,4 +14,11 @@ public class Random extends java.util.Random
 		int randomInt = nextInt(0xffffff);
 		return randomInt + (0xff << 24);
 	}
+	
+	public int nextInt(int from, int to)
+	{
+		Random r = new Random();
+		int range = to - from + 1;
+		return (r.nextInt(range)) + from;
+	}
 }
