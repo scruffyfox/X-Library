e0b019a71ebd39b07ee4749140ed603335474025
Callum Taylor <callum@3sidedcube.com>
2012-11-19 11:22:28 +0000

    Added hierarchy dump in ViewUtils

--- src/x/util/ViewUtils.java
+++ src/x/util/ViewUtils.java
@@ -1,9 +1,12 @@
 /**
  * @brief x util is the utility library which includes the method extentions for common data types
- * 
+ *
  * @author Callum Taylor
 **/
 package x.util;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
 
 import x.type.ItemList;
 import android.view.View;
@@ -14,9 +17,61 @@ import android.view.ViewGroup;
  */
 public class ViewUtils
 {
+	private static HashMap<Integer, String> mIdValues;
+	private static void getIdValues(Class R)
+	{
+		mIdValues = new HashMap<Integer, String>();
+		for (Field f : R.getFields())
+		{
+			try
+			{
+				mIdValues.put((Integer)R.getField(f.getName()).get(Integer.class), f.getName());
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Dumps the hierarchy of a view
+	 * @param R The ID reference of your project, must be R.id.class
+	 * @param depth The initial tab depth (usually 0)
+	 * @param parent The root view
+	 * @return A line seperated, tabbed structure of the view's hierarchy
+	 */
+	public static String dump(Class R, int depth, View parent)
+	{
+		if (mIdValues == null)
+		{
+			getIdValues(R);
+		}
+
+		String retStr = "";
+		retStr += "\n" + (depth == 0 ? "" : (StringUtils.padTo("", depth, "\t"))) + parent.toString();
+
+		if (mIdValues.get(parent.getId()) != null)
+		{
+			retStr += " id: R.id." + mIdValues.get(parent.getId());
+		}
+
+		if (parent instanceof ViewGroup)
+		{
+			int childCount = ((ViewGroup)parent).getChildCount();
+
+			for (int index = 0; index < childCount; index++)
+			{
+				retStr += dump(R, depth + 1, ((ViewGroup)parent).getChildAt(index));
+			}
+		}
+
+		return retStr;
+	}
+
 	/**
 	 * Gets all views of a parent that match an instance (recursive)
-	 * @param parent The parent view 
+	 * @param parent The parent view
 	 * @param instance The instance to check
 	 * @return An array of views, or null
 	 */
@@ -24,11 +79,11 @@ public class ViewUtils
 	{
 		ItemList<View> views = new ItemList<View>();
 		int childCount = parent.getChildCount();
-		
+
 		for (int childIndex = 0; childIndex < childCount; childIndex++)
 		{
 			View child = parent.getChildAt(childIndex);
-			
+
 			if (child instanceof ViewGroup)
 			{
 				views.add(getAllChildrenByInstance((ViewGroup)child, instance));
@@ -41,10 +96,10 @@ public class ViewUtils
 				}
 			}
 		}
-		
+
 		return views.size() > 0 ? (View[])views.toArray(new View[views.size()]) : null;
 	}
-	
+
 	/**
 	 * Gets the first child it finds in a parent matched from an instance (recursive)
 	 * @param parent The parent view
@@ -52,30 +107,30 @@ public class ViewUtils
 	 * @return The found view, or null
 	 */
 	public static View getFirstChildByInstance(ViewGroup parent, Class instance)
-	{		
+	{
 		View retView = null;
 		int childCount = parent.getChildCount();
-		
+
 		for (int childIndex = 0; childIndex < childCount; childIndex++)
 		{
 			View child = parent.getChildAt(childIndex);
-			
+
 			if (child.getClass() == instance)
 			{
 				return child;
 			}
-			
+
 			if (child instanceof ViewGroup)
 			{
 				View v = getFirstChildByInstance((ViewGroup)child, instance);
-				
+
 				if (v != null)
 				{
 					return v;
 				}
-			}									
+			}
 		}
-		
+
 		return retView;
 	}
 }
